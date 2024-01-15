90d11d07861988251a23ed0fd836da13b726d4a9
Kuber-Pathak <kuberpathak124@gmail.com>
2024-01-15 21:32:10 +0545

    modified

---
+++ liz/signup.js
@@ -0,0 +1,55 @@
+function DoAnimation() {
+  var targetaElement = document.getElementById("targeta");
+  targetaElement.className = "animate1";
+
+  var targetbElement = document.getElementById("targetb");
+  targetbElement.className = "animate2";
+
+  var targetcElement = document.getElementById("targetc");
+  targetcElement.className = "animate3";
+
+  var targetdElement = document.getElementById("targetd");
+  targetdElement.className = "animate4";
+}
+
+function RevAnimation() {
+  var targetaElement = document.getElementById("targeta");
+  targetaElement.className = "animate5";
+
+  var targetbElement = document.getElementById("targetb");
+  targetbElement.className = "animate6";
+
+  var targetcElement = document.getElementById("targetc");
+  targetcElement.className = "animate7";
+
+  var targetdElement = document.getElementById("targetd");
+  targetdElement.className = "animate8";
+}
+
+function subanimation() {
+  var targetaElement = document.getElementById("targeta");
+  targetaElement.className = "animate9";
+
+  var targetbElement = document.getElementById("targetb");
+  targetbElement.className = "animate10";
+
+  var targetcElement = document.getElementById("targetc");
+  targetcElement.className = "animate11";
+
+  var targetdElement = document.getElementById("targetd");
+  targetdElement.className = "animate12";
+}
+
+function dubanimation() {
+  var targetaElement = document.getElementById("targeta");
+  targetaElement.className = "animate13";
+
+  var targetbElement = document.getElementById("targetb");
+  targetbElement.className = "animate14";
+
+  var targetcElement = document.getElementById("targetc");
+  targetcElement.className = "animate15";
+
+  var targetdElement = document.getElementById("targetd");
+  targetdElement.className = "animate16";
+}
